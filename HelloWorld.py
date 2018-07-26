166f8885b1ceb958fdb15a5d22f90200e4f78173
Subhas Ch Bera <subhas.9874@gmail.com>
2018-07-26 22:56:36 +0530

    pulled from the remote

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+#print('Hello World, Hello Subhas')
+#print('How are you?')
+
+print('Hello World, Hello Subhas \nHow are you?') # \n for next line
+print('How was the day?')
+print('Lets do some Pythoning')
