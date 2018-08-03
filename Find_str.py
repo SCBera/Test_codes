7d1665119ef19cdc4fa0e4100c071610989324c0
SCBera <subhas.9874@gmail.com>
2018-08-03 19:41:09 +0530

    indexing

---
+++ Find_str.py
@@ -0,0 +1,7 @@
+#Finding a perticular string in a input
+
+a = input()
+print(a.find('+')) #returns the position (counted from 0)
+print(len(a)) #returns total no of str
+print(a.split()) #
+print(float(a.split()))
