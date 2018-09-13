1234e1ca76ec3b18213d449de88b6bb8380f8752
SCBera <subhas.9874@gmail.com>
2018-09-13 12:01:44 +0530

    working in TIFR-PC, intensity vary

--- readtif.py
+++ readtif.py
@@ -1,14 +1,14 @@
 from skimage import io
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 dir = input('Dir>')
+Dir = dir.replace('\\', '/')
 # print(dir)
+# print(Dir)
 
-##Dir = input('Dir>')
-##FileName = input('filename>')
-stack_pos = input('stack_pos>')
+stack_pos = input('stack_position>')
 number_of_files = input('How many files to read>')
 
 listfiles = []
@@ -22,26 +22,27 @@ for img_files in os.listdir(dir):  # gets the file names
 
 
 # print(len(listfiles))
-# print(listfiles)
-Dir = dir.replace('\\', '/')
-# print(Dir)
+#print(listfiles)
+
 img = io.imread(Dir+listfiles[1])
 im_new = (img[int(stack_pos)])  # gets the np array of the image of index i
 print(im_new)
 # io.imshow(img[int(stack_pos)])
 
-stack = np.zeros((int(number_of_files), img.shape[1], img.shape[2]), np.uint16)
+#stack = np.zeros((int(number_of_files), img.shape[1], img.shape[2]), np.uint16)
+stack = np.zeros((int(number_of_files), img.shape[1], img.shape[2]), np.float32)
+
 
 for n in range(0, len(listfiles)):
     img = io.imread(Dir+listfiles[n])
-    # gets the np array of the image of index i
     new_img = (img[int(stack_pos)])
     stack[n, :, :] = new_img
 
-# io.imshow(stack[0])
+
+print(stack.shape)
 
 # path_results = '..Part_A_Results/'
-#io.imsave(Dir + 'Stack_of_slice_' + stack_pos + '.tif', stack)
+io.imsave(f"{Dir}Stack_of_slice_{stack_pos}_first_{number_of_files}_files.tif", stack)
 
 #new_stack = []
 #
@@ -54,7 +55,8 @@ for n in range(0, len(listfiles)):
 #
 #im = io.imread(Dir + FileName + '.tif')
 #im = io.imread('D:/Codes/test.tif')
-# print(im.shape) # prints the z, y, x dimention of the image file
+#print(img.shape) # prints the z, y, x dimention of the image file
+#print(im_new.shape) # prints the z, y, x dimention of the image file
 # print(im.shape[0]) # shape[i] where i=0 for Z, i=1 for y and i=2 for x
 
 # io.imshow(im_new) # shows the image
