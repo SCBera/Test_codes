c6057f6f6b56121db6a62e3730f4a2c109c550d5
SCBera <subhas.9874@gmail.com>
2018-09-18 17:40:55 +0530

    mean working

--- readtif_multi.py
+++ readtif_multi.py
@@ -12,8 +12,6 @@ import os
 import glob
 #import sys
 
-#files = glob.glob(Dir+filetype)
-#print(files)
 def get_dir(dir_):
     dir_ = dir_ + "\\"
     return dir_
@@ -38,18 +36,6 @@ def read_stack(file):
     image = io.imread(file)
     return image
 
-def read_slice_t(image, t):
-    slice_t = image[t]
-    return slice_t
-
-def get_max(stack):
-    stack_max = np.max(stack, axis = 0)
-    return stack_max
-
-def get_mean(stack):
-    stack_mean = np.mean(stack, axis = 0)
-    return stack_mean
-
 def save_tif(dir_out, list_of_files, slice_t, result, mode):
     try:
         io.imsave(f"{dir_out}{mode}_of_slice-{slice_t + 1}_from_{len(list_of_files)}-files.tif", result)
@@ -59,7 +45,7 @@ def save_tif(dir_out, list_of_files, slice_t, result, mode):
 def save_csv(dir_out, list_of_files, result):
     try:
         np.savetxt(f"{dir_out}Results_from_{len(list_of_files)}-files.csv",
-                   result.T, delimiter=",", header='Time, Avrg_int, SD, SE')
+                   result.T, delimiter=",", header='Time, Avrg_int, SD, SE, Sum_int')
     except:
         print("Existing csv file is not accessible!")
 
@@ -96,6 +82,7 @@ if __name__ == "__main__":
     img = read_stack(list_of_files[0])
     new_stack = np.zeros((len(list_of_files), img.shape[1], img.shape[2]), img.dtype)
     list_of_mean = []
+    list_of_sum = []
     list_of_sd = []
     list_of_sem = []
     t_points = []
@@ -107,25 +94,34 @@ if __name__ == "__main__":
             img = read_stack(list_of_files[n])
             if img.shape[0] < 3:
                 continue
-            frame = read_slice_t(img, slice_t)
+            frame = img[slice_t]
             # making a new set of stacks for each time point
             new_stack[n, :, :]= frame
             # saving the newly made stack
 #            save_tif(dir_out, list_of_files, slice_t, new_stack)
 
         list_of_mean.append(new_stack.mean())
+        list_of_sum.append(new_stack.sum())
         list_of_sd.append(new_stack.std())
         list_of_sem.append(new_stack.mean()/math.sqrt(len(list_of_files)))
 
-        mean_of_stacks = np.mean(new_stack, axis = 0)
+        mean_of_stacks = np.mean(new_stack, axis = 0).astype(int) # converts float array to trancated int (eg., 2.9 to 2)
+#        mean_of_stacks = np.mean(new_stack, axis = 0).astype(np.float16) # converts 16bit float
+#        mean_of_stacks = np.rint(np.mean(new_stack, axis = 0)) # rounding float to float
+
+        sum_of_stacks = np.sum(new_stack, axis = 0)
 
         max_of_stacks = np.max(new_stack, axis = 0)
-        # saving the mean of the stacks
-        save_tif(dir_out, list_of_files, slice_t, max_of_stacks, 'Max')
-#        save_tif(dir_out, list_of_files, slice_t, mean_of_stacks, 'Mean')
-        result_csv = np.array([t_points, list_of_mean, list_of_sd, list_of_sem])
+        print(mean_of_stacks)
 
-        print(new_stack.mean(), t_points)
+        # saving the calculated stacks
+        save_tif(dir_out, list_of_files, slice_t, max_of_stacks, 'Max')
+        save_tif(dir_out, list_of_files, slice_t, mean_of_stacks, 'Mean')
+        save_tif(dir_out, list_of_files, slice_t, sum_of_stacks, 'Sum')
+
+        result_csv = np.array([t_points, list_of_mean, list_of_sd, list_of_sem, list_of_sum])
+
+    print(new_stack.sum(), t_points)
     save_csv(dir_out, list_of_files, result_csv)
     plot_save_fig(dir_out, list_of_files, result_csv)
 
