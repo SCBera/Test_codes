4c1906ed1041ee37462f699392dc7849babde472
Subhas Ch Bera <subhas.9874@gmail.com>
2018-09-23 00:11:18 +0530

    split test ok

--- read.py
+++ read.py
@@ -5,9 +5,14 @@ print("opening file..")
 n = 0
 for line in file_:
     n += 1
-    print(f"line-{n}: {line}")
-    if n % 2 != 0:
-        dir_ = line
-    else:
-        t_point = int(line)
-print(f"Dir: {dir_}\ntime: {t_point}")
+    # print(f"line-{n}: {line}")
+
+    line = line.split(',')
+
+    print(f"line-{n}: {line[0]}, {float(line[1])}")
+
+#     if n % 2 != 0:
+#         dir_ = line
+#     else:
+#         t_point = int(line)
+# print(f"Dir: {dir_}\ntime: {t_point}")
