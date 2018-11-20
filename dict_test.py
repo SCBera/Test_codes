addae06ecae811ac778a77bbdd3ea6180ec25133
SCBera <subhas.9874@gmail.com>
2018-11-20 15:09:09 +0530

    min diff added

--- dict_test.py
+++ dict_test.py
@@ -1,7 +1,12 @@
 
-peaks = [10, 15, 30, 50]
+peaks = [10, 15, 30, 50, 52, 70]
 
-peaks_int = [0.5, 0.9, 1.2, 0.1]
+peaks_int = [0.5, 0.9, 1.2, 0.1, 1.3, 1.9]
+
+#finds the minimum difference between two adjucent elements progresively
+print(min([abs(x-y)for x, y in zip(peaks[:-1],peaks[1:])]))
+#finds the minimum difference between two pair of elements
+print(min([abs(x-y)for x,y in zip(peaks[::2],peaks[1::2])]))
 
 dict_peak = {}
 for (peak_int, peak) in zip(peaks_int, peaks):
