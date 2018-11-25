70f8a0d412ce91895a75e334cf3c9608666b732f
SCBera <subhas.9874@gmail.com>
2018-11-25 17:06:29 +0530

    scipy optimize working with initial guess

--- fit_test.py
+++ fit_test.py
@@ -13,18 +13,18 @@ size = 200
 xdata = np.linspace(-10, 10, size)
 
 ## generate ydata from xdata with some noise added in next step
-ydata1 = 2*np.sin(np.pi*xdata*0.5)
-ydata2 = ydata1 + np.random.normal(scale=0.2, size=size)
+ydata1 = 1*np.sin(np.pi*xdata*0.1)
+ydata2 = ydata1 + np.random.normal(scale=0.05, size=size)
 
 ## plot data
-# plt.plot(xdata, ydata1) #plots blue line
-plt.scatter(xdata, ydata2, c='gray', marker='o', label="data")
+plt.plot(xdata, ydata1, label="data") #plots blue line
+plt.scatter(xdata, ydata2, c='gray', marker='o', label="data with noise")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
 plt.legend(loc='upper left') #label showing after using this
 
 ## fit the data and get the parameters
-fit_data = optimize.curve_fit(sin_func, xdata, ydata2)
+fit_data = optimize.curve_fit(sin_func, xdata, ydata2, p0=[1,0.3])
 
 a, b = fit_data[0]
 
@@ -33,7 +33,7 @@ plt.plot(xdata, sin_func(xdata, a, b), c='r', linestyle='dashed', label="fit") #
 # plt.Line2D(xdata, sin_func(xdata, a, b), color='r', linestyle='dashed', label="fit")
 plt.legend(loc='upper left') #label showing after using this
 
-# print(fit_data)
+print(fit_data[0])
 
 plt.show()
 
