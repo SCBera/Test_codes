b6ce9c4d7574589133a3d5716683e7a0b9403493
SCBera <subhas.9874@gmail.com>
2018-11-25 18:36:03 +0530

    using lmfit..

--- fit_test.py
+++ fit_test.py
@@ -1,10 +1,22 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
 from scipy import optimize
+from lmfit import Model
 
-def sin_func(x, a, b):
+def sine_func(x, a, b):
     return a*np.sin(b*x)
+
+def fit_scipy_opt(xdata, ydata):
+    fit_params = optimize.curve_fit(sine_func, xdata, ydata, p0=[1,0.3])
+    return fit_params[0]
+
+def fit_lm(xdata, ydata):
+    sinemodel = Model(sine_func)
+    params = sinemodel.make_params(a=1, b=0.1)
+    result = sinemodel.fit(ydata, params, x=xdata)
+    # result = sinemodel.fit(ydata, x=xdata, a=1, b=0.1) #params not required this way
+
+    return result
 
 np.random.seed(0)
 
@@ -12,7 +24,7 @@ size = 200
 ## generate xdata
 xdata = np.linspace(-10, 10, size)
 
-## generate ydata from xdata with some noise added in next step
+## generate ydata from xdata and added some noise
 ydata1 = 1*np.sin(np.pi*xdata*0.1)
 ydata2 = ydata1 + np.random.normal(scale=0.05, size=size)
 
@@ -24,16 +36,14 @@ plt.ylabel("Amplitude")
 plt.legend(loc='upper left') #label showing after using this
 
 ## fit the data and get the parameters
-fit_data = optimize.curve_fit(sin_func, xdata, ydata2, p0=[1,0.3])
-
-a, b = fit_data[0]
+a, b = fit_scipy_opt(xdata, ydata2)
+reslt = fit_lm(xdata, ydata2)
+print(a, b, reslt.fit_report())
 
 ## plot fitted data on original data
-plt.plot(xdata, sin_func(xdata, a, b), c='r', linestyle='dashed', label="fit") #label not showing
-# plt.Line2D(xdata, sin_func(xdata, a, b), color='r', linestyle='dashed', label="fit")
+# plt.plot(xdata, sine_func(xdata, a, b), c='r', linestyle='dashed', label="fit") #label not showing
+plt.plot(xdata, reslt.best_fit, c='r', linestyle='dashed', label="fit") #label not showing
 plt.legend(loc='upper left') #label showing after using this
-
-print(fit_data[0])
 
 plt.show()
 
