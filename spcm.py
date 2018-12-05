72cc9e4abb8382001d4be6517f911853567a1510
SCBera <subhas.9874@gmail.com>
2018-12-05 19:02:33 +0530

    pxl recognition added

--- spcm.py
+++ spcm.py
@@ -4,10 +4,13 @@ This program is for automatic conversion of FCS data file acquired in BH.
 Author: Subhas Chandra Bera, last updated, 27th Novenber, 2018
 
 """
+# from skimage import io
+import matplotlib.pyplot as plt
 import pyautogui
 import pyperclip
 import glob
 import time
+import sys
 
 
 def MoveWin(x, y):
@@ -38,6 +41,7 @@ def type_(dir_in):
     # pyautogui.typewrite('Hello world!')
     pyautogui.typewrite(dir_in)
 
+
 if __name__ == "__main__":
     print("This is tested while \'DPC-230 Emulation - FIFO data files conversion\'window\
      is open and top of other windows. Please test it before actual use!\n")
@@ -49,6 +53,10 @@ if __name__ == "__main__":
     files_set = glob.glob(dir_+'*.set')
     files_spc = glob.glob(dir_+'*.spc')
 
+    if len(files_set) == 0:
+        print(" No such file in the directory!")
+        exit()
+
 # starting position of the working window.
 # other positions are relative, so handled automatically!
     x = 50
@@ -58,41 +66,48 @@ if __name__ == "__main__":
                     # at 100, 100 position (top left corner).
 
     MouseMoveClick(x+50, x)
+    img1 = pyautogui.pixel(x+290,y+365)
 
     for (file_set, file_spc) in zip(files_set, files_spc):
+        img2 = pyautogui.pixel(x+290,y+365)
+        # print(img1==img2)
+        while img1 != img2:
+            time.sleep(2)
+            img2 = pyautogui.pixel(x+290,y+365)           
 
-            dir_set = file_set
-            MouseMove(x+300, y+65) #setup filename position
-            Mouse3click('left', 0.1)
-            # type_(dir_set) #type may take long time for longer pathname
-            pyperclip.copy(dir_set)
-            pyautogui.hotkey("ctrl", "v") #faster
-            time.sleep(0.2) #in second
+        dir_set = file_set
+        MouseMove(x+300, y+65) #setup filename position
+        Mouse3click('left', 0.1)
+        # type_(dir_set) #type may take long time for longer pathname
+        pyperclip.copy(dir_set)
+        pyautogui.hotkey("ctrl", "v") #faster
+        # time.sleep(0.2) #in second
 
-            dir_spc = file_spc
-            MouseMove(x+300, y+315) #source filename position
-            Mouse3click('left', 0.1)
-            # type_(dir_spc) #type may take long time for longer pathname             
-            pyperclip.copy(dir_spc)
-            pyautogui.hotkey("ctrl", "v") #faster
-            time.sleep(0.2) #in second
+        dir_spc = file_spc
+        MouseMove(x+300, y+315) #source filename position
+        Mouse3click('left', 0.1)
+        # type_(dir_spc) #type may take long time for longer pathname             
+        pyperclip.copy(dir_spc)
+        pyautogui.hotkey("ctrl", "v") #faster
+        # time.sleep(0.2) #in second
 
-            MouseMove(x+300, y+475) #source filename position
-            Mouse3click('left', 0.1)
-            # type_(dir_spc) #type may take long time for longer pathname             
-            pyperclip.copy(dir_spc[:-4]+'_cnvrtd.sdt')
-            pyautogui.hotkey("ctrl", "v") #faster
-            time.sleep(0.2) #in second
+        MouseMove(x+300, y+475) #source filename position
+        Mouse3click('left', 0.1)
+        # type_(dir_spc) #type may take long time for longer pathname             
+        pyperclip.copy(dir_spc[:-4]+'_cnvrtd.sdt')
+        pyautogui.hotkey("ctrl", "v") #faster
+        # time.sleep(0.2) #in second
 
+        # im = pyautogui.screenshot(region=(260,400, 90, 50))
+        # img1 = im.getpixel((320,430))
+        
 
-
-
-            MouseMoveClick(x+130, y+660)
-            print("Converting file...")
-            # print(dir_set)
-            # print(dir_spc)
-            ## carefully choose this to avoid file skipping for longer conversion time
-            time.sleep(1) #in second
+        MouseMoveClick(x+130, y+660)
+        print("Converting file...")
+        # print(dir_set)
+        # print(dir_spc)
+        ## carefully choose this to avoid file skipping for longer conversion time
+        # time.sleep(5) #in second
             
     print("Done!")
 
