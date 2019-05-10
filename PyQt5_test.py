a75f876fccc360eaff1bc01dadfa8a5d9c8b5e78
Subhas Ch Bera <subhas.9874@gmail.com>
2019-05-10 21:07:06 +0200

    lession 3_push btn not working

--- PyQt5_test.py
+++ PyQt5_test.py
@@ -1,20 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QCoreApplication
 
 
 class window(QMainWindow):
 
     def __init__(self):
         super(window, self).__init__()
-        self.setGeometry(1000, 200, 300, 200)
+        self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle('Test!')
-        self.setWindowIcon(QIcon('Penguins.jpg'))
-        self.show()
+        # self.setWindowIcon(QIcon('Penguins.jpg'))
+        self.home()
+
+    def home(self):
+        btn = QPushButton('Quit', self)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.resize(100, 100)
+        btn.move(100, 100)
+        btn.show()
 
 
-app = QApplication(sys.argv)
+def run():
+    app = QApplication(sys.argv)
+    Gui = window()
+    sys.exit(app.exec_())
 
-GUI = window()
 
-sys.exit(app.exec_())
+run()
